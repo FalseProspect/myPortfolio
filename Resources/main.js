fd0aec2858cbe9276119a7af96ad8452b4a92863
Keith <andrerose722@gmail.com>
2018-12-10 16:39:35 -0500

    added projectoverlay and missing images

--- Resources/main.js
+++ Resources/main.js
@@ -7,6 +7,14 @@ let lightMode = false;
 //Prospect Text
 let prospect = 'PROSPECT';
 let textChar = 0;
+//Projects
+let projects = [
+  {name:'myDay', image:'Resources/Media/myDayTitleLogo1080.png', color: '#4F54FF'},
+  {name:'myTask', image:'Resources/Media/myTaskTitleLogo1080.png', color: '#F3A712'},
+  {name:'Bleep-Space', image:'Resources/Media/BSTitle1to1.png', color: 'var(--accentColor)'},
+  {name:'Intruder', image:'Resources/Media/IntruderTitle1-1.png', color: 'var(--accentColor)'},
+  {name:'Bad Battle', image:'Resources/Media/BadBattleTitle1-1.png', color: 'var(--accentColor)'}
+]
 
 document.body.focus();
 
@@ -36,6 +44,44 @@ setTimeout(()=>{
 window.addEventListener('resize', ()=>{
   resizeItems();
 })
+
+
+
+function makeElement(elemTag, classname, id){
+  let elem = document.createElement(elemTag);
+  classname ? elem.setAttribute('class',classname) : 0;
+  id ? elem.id = id : 0; 
+  return elem;
+}
+
+function createItems(){
+  let container = document.getElementById('container')
+
+
+  projects.forEach((proj)=>{
+    let item = makeElement('div','item');
+
+    let overlay = makeElement('div','itemOverlay')
+    overlay.style.background = proj.color;
+
+    let name = makeElement('h1','itemName');
+    name.innerText = "This is name";
+    let description = makeElement('h2','itemDescription');
+    description.innerText = "This is description";
+    overlay.appendChild(name);
+    overlay.appendChild(description);
+
+    let image = makeElement('img')
+    image.setAttribute('src',proj.image);
+    item.appendChild(overlay);
+    item.appendChild(image);
+    container.appendChild(item);
+  })
+
+  
+}
+
+createItems()
 
 window.onload = resizeItems()
 
